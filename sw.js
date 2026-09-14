179de2dfbdeac6f489de7ae13372cb71fc52626d
Hh-oi <lei.vill.20@gmail.com>
2026-09-14 15:22:03 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'agrogestion-v2';
+const CACHE_NAME = 'agrogestion-v3';
 self.addEventListener('install', (e) => {
     self.skipWaiting();
     e.waitUntil(
@@ -8,8 +8,21 @@ self.addEventListener('install', (e) => {
     );
 });
 self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
+    e.waitUntil(
+        caches.keys().then((keys) => 
+            Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))
+        ).then(() => self.clients.claim())
+    );
 });
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(
+        fetch(e.request).then((networkResponse) => {
+            return caches.open(CACHE_NAME).then((cache) => {
+                cache.put(e.request, networkResponse.clone());
+                return networkResponse;
+            });
+        }).catch(() => {
+            return caches.match(e.request);
+        })
+    );
 });
